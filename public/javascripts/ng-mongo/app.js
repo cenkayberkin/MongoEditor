2afc0bc3cd4302e4ffab6794ae73f40f69944175
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-03-09 20:42:15 +0200

    Mongo Service Added

--- public/javascripts/ng-mongo/app.js
+++ public/javascripts/ng-mongo/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by cenk on 3/9/14.
  */
+var ngMongo = angular.module('ngMongo',[]);
+
+ngMongo.factory('Mongo',function($http){
+  return {
+    database:function(){
+      return $http.get('/mongo-api/dbs');
+    }
+  }
+});
+
+ngMongo.controller('ListCtrl' , function($scope,Mongo){
+  var result = Mongo.database();
+  result.success(function (data){
+    $scope.items = data;
+  });
+});
